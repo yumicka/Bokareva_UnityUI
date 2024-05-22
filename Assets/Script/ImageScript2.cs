91d52f3c1e8b8a061e3b438ff5d1c761d64c472a
Alina Bokareva <alina.bokareva@sk.lvt.lv>
2024-05-22 21:33:32 +0300

    Papilginaju scrool view

--- Assets/Script/ImageScript2.cs
+++ Assets/Script/ImageScript2.cs
@@ -12,6 +12,7 @@ public class ImageScript : MonoBehaviour
     public GameObject imageHolder;
     public GameObject sizeSlider;
     public GameObject rotationSlider;
+    public GameObject scroll;
 
     public void showHelmet1(bool value)
     {
@@ -33,11 +34,18 @@ public class ImageScript : MonoBehaviour
         if (index >= 0 && index < spriteArray.Length)
         {
             imageHolder.GetComponent<Image>().sprite = spriteArray[index];
-            // Устанавливаем размер изображения в соответствии с оригинальным размером спрайта
             imageHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(
                 spriteArray[index].rect.width,
                 spriteArray[index].rect.height
             );
+            if(index == 0) {
+                scroll.GetComponent<Text>().text = "Alex is a girl about to goon a dangerous adventure! Many biomes, challenges and monsters are waiting for her. Help her to choose a good outfit so that she can face all the adventures with courage!";
+            }
+            
+            else if(index == 1) {
+                scroll.GetComponent<Text>().text = "Yuma is a guy who likes fire and danger. His favourite biome is the Nether, where he lives and travels around exploring Bastions and Fortresses. Pretty soon Yuma will have to go to the depths of Nether again, I think he needs the right clothes for that...";
+            }
+
         }
     }
 
