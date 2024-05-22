69350338b92ec194f4df2832f55d6ba07026d8af
Alina Bokareva <alina.bokareva@sk.lvt.lv>
2024-05-22 20:30:38 +0300

    Projekta atjaunosana un papildinasana

--- Assets/Script/ImageScript2.cs
+++ Assets/Script/ImageScript2.cs
@@ -30,23 +30,27 @@ public class ImageScript : MonoBehaviour
 
     public void changeImage(int index)
     {
-        if (index == 0)
-            imageHolder.GetComponent<Image>().sprite = spriteArray[0];
-
-        else if(index == 1)
-            imageHolder.GetComponent<Image>().sprite = spriteArray[1];
+        if (index >= 0 && index < spriteArray.Length)
+        {
+            imageHolder.GetComponent<Image>().sprite = spriteArray[index];
+            // Устанавливаем размер изображения в соответствии с оригинальным размером спрайта
+            imageHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(
+                spriteArray[index].rect.width,
+                spriteArray[index].rect.height
+            );
+        }
     }
 
 
     public void changeSize()
     {
         float size = sizeSlider.GetComponent<Slider>().value;
-        imageHolder.transform.localScale = new Vector2(1F * size, 1F * size);
+        imageHolder.transform.localScale = new Vector2(size, size);
     }
 
     public void changeRotation()
     {
         float rotation = rotationSlider.GetComponent<Slider>().value;
-        imageHolder.transform.localRotation = Quaternion.Euler(0, 0, rotation*360);
+        imageHolder.transform.localRotation = Quaternion.Euler(0, 0, rotation * 360);
     }
 }
