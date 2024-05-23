6114a14289aa780914d503b86e511db8ada40a24
Alina Bokareva <alina.bokareva@sk.lvt.lv>
2024-05-23 21:30:05 +0300

    Papildināju programmu ar text

---
+++ Assets/Script/ShowTextScrip1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextScrip : MonoBehaviour
+{
+    string vards;
+    int gads;
+    int vecums;
+    public GameObject inputField;
+    public GameObject inputField2;
+    public GameObject displayField;
+
+    public void getText(){
+        vards = inputField.GetComponent<InputField>().text;
+
+        if (int.TryParse(inputField2.GetComponent<InputField>().text, out gads)) {
+            vecums = 2024 - gads;
+            displayField.GetComponent<Text>().text = vards + " ir " + vecums + " gadi!";
+        }
+
+}
+}
